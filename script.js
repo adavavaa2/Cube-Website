4d944269937ccbfcda8a252c29035a5eeb0d0f99
adavavaa2 <raffaelkrieger@gmail.com>
2026-07-30 01:11:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Cube Website gestartet");
+
+
+const button = document.querySelector("button");
+
+
+button.addEventListener("click", () => {
+
+    alert(
+        "Der Cube Client kommt bald!"
+    );
+
+});
